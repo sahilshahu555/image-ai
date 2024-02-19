1e107f14e0f806936cca358164d9f9c987b3ecd3
sahilshahu555 <sahilshahu555@gmail.com>
2024-02-19 22:32:33 +0530

    created Clerk auth

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Layout=({children}:{children:React.ReactNode}) =>{
     return(
-        <main className="auth" >{children}</main>
+        <main className="auth " >{children}</main>
     )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from '@clerk/nextjs'
 
 const IBM_Plex = IBM_Plex_Sans({
   subsets: ["latin"],
@@ -20,10 +21,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn("font-IBM_PLEX antialiased",IBM_Plex.variable)}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider appearance={{
+      variables: { colorPrimary: '#624cf5' }
+    }}>
+      <html lang="en">
+        <body className={cn("font-IBM_PLEX antialiased",IBM_Plex.variable)}
+        suppressHydrationWarning={true}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
