8f789ea51f2fdc74327130856dada047c5dc4e88
sahilshahu555 <sahilshahu555@gmail.com>
2024-02-22 14:20:40 +0530

    edited css

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -14,16 +14,16 @@ const Home = async ({ searchParams }: SearchParamProps) => {
     <>
       <section className="home">
         <h1 className="home-heading">
-          Show Creative Vision with Imaginify
+          Show Creative Vision with ImageAI
         </h1>
-        <ul className="flex-center w-full gap-20">
+        <ul className="flex-center w-full gap-6 md:gap-20">
           {navLinks.slice(1, 5).map((link) => (
             <Link
               key={link.route}
               href={link.route}
-              className="flex-center flex-col gap-2"
+              className="flex-center flex-col  gap-2"
             >
-              <li className="flex-center w-fit rounded-full bg-white p-4">
+              <li className="flex-center w-fit rounded-full bg-white p-1 md:p-4">
                 <Image src={link.icon} alt="image" width={24} height={24} />
               </li>
               <p className="p-14-medium text-center text-white">{link.label}</p>
